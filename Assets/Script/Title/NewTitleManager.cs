3dcc5eefc5a806ee235709eb8d3cb68bc8d62ce9
dkswhdgh1018 <zzongho1018@gmail.com>
2025-11-03 22:59:46 +0900

    11-03-01
    Title

---
+++ Assets/Script/Title/NewTitleManager.cs
@@ -0,0 +1,32 @@
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NewTitleManager : MonoBehaviour
+{
+    public Text pressEnterText;
+
+    void Start()
+    {
+        StartCoroutine(BlinkText());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
+    IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            pressEnterText.enabled = !pressEnterText.enabled;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
