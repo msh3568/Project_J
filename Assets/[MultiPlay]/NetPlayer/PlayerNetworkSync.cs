280fd383ec63865a52edc2cf8c747eeb26d3513e
STEMULE <stemple0@naver.com>
2026-01-04 11:58:16 +0900

    PlayerNetworkSync 코드 정리

--- Assets/[MultiPlay]/NetPlayer/PlayerNetworkSync.cs
+++ Assets/[MultiPlay]/NetPlayer/PlayerNetworkSync.cs
@@ -1,45 +1,65 @@
 ﻿using Fixer;
 using UnityEngine;
 
-[RequireComponent(typeof(Transform))]
+// Player의 상태(위치, 방향, 애니메이션 상태)를 서버로 전송
+// 멀티플레이에서 사용, 싱글플레이에서는 작동 X
 public class PlayerNetworkSync : MonoBehaviour
 {
     private StateMachine _stateMachine;
-    private Transform _tr;
-
     private Player player;
 
     private void Awake()
     {
-        _tr = transform;
-
         player = GetComponent<Player>();
         _stateMachine = player != null ? player.GetStateMachine() : null;
     }
 
     private void OnEnable()
     {
-        FixerClient.Instance?.BindLocalPlayerSync(this);
+        if (FixerClient.Instance)
+        {
+            Debug.Log("PlayerNetworkSync : Player 상태 서버 전송 가능 (멀티플레이 모드)");
+            FixerClient.Instance.BindLocalPlayerSync(this);
+        }
+        else
+        {
+            Debug.Log("PlayerNetworkSync : Player 상태 서버 전송 불가. (싱글플레이 모드)");
+        }
+        
     }
 
     private void OnDisable()
     {
-        FixerClient.Instance?.UnbindLocalPlayerSync(this);
+        if (FixerClient.Instance)
+        {
+            FixerClient.Instance.UnbindLocalPlayerSync(this);
+        }
     }
 
     // FixerClient가 호출해서 전송에 사용
     public CharacterState CollectSnapshot()
     {
         CharacterState state = new CharacterState();
-        state.PosX = _tr.position.x;
-        state.PosY = _tr.position.y;
-        state.FacingDir = (sbyte)(_tr.localScale.x >= 0 ? 1 : -1);
 
-        PlayerStateForNetwork netState = PlayerStateForNetwork.Idle;
-        if (_stateMachine != null)
-            netState = _stateMachine.CurrentNetworkState;
+        if (FixerClient.Instance)
+        {
+            state.PosX = transform.position.x;
+            state.PosY = transform.position.y;
+            state.FacingDir = (sbyte)(transform.localScale.x >= 0 ? 1 : -1);
 
-        state.ActionState = (byte)netState;
+            PlayerStateForNetwork netState = PlayerStateForNetwork.Idle;
+            if (_stateMachine != null)
+                netState = _stateMachine.CurrentNetworkState;
+
+            state.ActionState = (byte)netState;
+
+            return state;
+        }
+        else
+        {
+            Debug.Log("[Error] PlayerNetworkSync : 서버로 Player 상태 전송 불가");
+            state = null;
+        }
 
         return state;
     }
